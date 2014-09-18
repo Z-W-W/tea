171846382beb6ecf00bd2bf2be361c0ac7350a68
Yunfei Zuo <yzuo@BELC02KK1L0FFT3.sea.corp.expecn.com>
2014-09-17 21:22:59 -0700

    initial checkin

---
+++ routes/teacher.js
@@ -0,0 +1,12 @@
+
+/*
+ * GET teacher listing.
+ */
+
+var fs = require('fs');
+var file = __dirname + '/teacher_list.json';
+var context = fs.readFileSync(file, 'utf8');
+
+exports.list = function(req, res){
+  res.send(context);
+};
