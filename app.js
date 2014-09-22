90db1d78152499c71a856f277a75a162f9d7acfa
Hao Wang <hao@wangfamily.info>
2014-09-22 13:11:07 -0700

    Add CORS support
    Enable CORS and remove JSONP

--- app.js
+++ app.js
@@ -10,13 +10,27 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set("jsonp callback", true);
+app.use(allowCrossDomain);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
