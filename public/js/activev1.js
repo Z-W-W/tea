1f8f688f2986fab9cd27b21e9963827b40095942
Hao Wang <hao@wangfamily.info>
2015-01-19 15:22:43 -0800

    Update base URL Remove logging and hook

--- public/js/activev1.js
+++ public/js/activev1.js
@@ -1,18 +1,6 @@
 /**
  * Created by hawang on 1/13/15.
  */
-var logs = {
-    sender: function (class0, method, ex) {
-        $.ajax({
-            url: '/LogReceiver/Script',
-            type: 'post',
-            data: { 'class': class0, 'method': method, 'ex': ex }
-
-        });
-
-    }
-
-};
 
 //活动主函数
 var active = function (u, a, v, p, l, i, b, t) {
@@ -38,11 +26,10 @@ var active = function (u, a, v, p, l, i, b, t) {
             };
             var wxCallbacks = {
                 favorite: false,
-                ready: function () { alert("This is a ready hook"); },
-                cancel: function () { alert("This is a cancel hook"); },
-                fail: function () { alert("This is a fail hook"); },
+                ready: function () { },
+                cancel: function () { },
+                fail: function () { },
                 confirm: function (resp) {
-                    alert("This is a confirm hook");
                     if (resp.err_msg === 'share_timeline:ok' || resp.err_msg === 'general_share:ok') {
                         $.ajax({
                             url: '/activev1/StartShare',
@@ -75,7 +62,6 @@ var active = function (u, a, v, p, l, i, b, t) {
         });
     }
     catch (e) {
-        logs.sender('active', 'active', e);
     }
 };
 
